e0a5b2c1f91246e43e99ac5f42076226aba692b4
Michael Karg <kargm@in.tum.de>
2013-04-19 22:38:09 +0200

    Added day month time year

--- exif_analyzer.py
+++ exif_analyzer.py
@@ -5,6 +5,7 @@ import os
 import string
 import csv
 import cairo
+import time
 
 from PIL import Image
 from PIL.ExifTags import TAGS
@@ -65,7 +66,12 @@ aperture_dict = {}
 exposure_dict = {}
 focallength_dict = {}
 iso_dict = {}
-
+day_dict = {"Monday" : 0, "Tuesday" : 0, "Wednesday" : 0, "Thursday" : 0, "Friday" : 0, "Saturday" : 0, "Sunday" : 0}
+month_dict = {}
+year_dict = {}
+weekday_mapping = {0 : "Monday", 1 : "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+month_mapping = {1 : "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 
+              10: "October", 11: "November", 12: "December"}
 
 for filename in os.listdir(dirname):
     full_file = dirname + filename
@@ -111,6 +117,31 @@ for filename in os.listdir(dirname):
                 else: 
                     iso_dict[str(iso)] = 1
 
+              
+                
+                if datetime != "Unknown":
+                    datetime_struct = time.strptime(datetime, "%Y:%m:%d %H:%M:%S")
+                    print("%s"%datetime_struct)
+                    day = weekday_mapping[datetime_struct.tm_wday]
+                    month = month_mapping[datetime_struct.tm_mon]
+                    year = datetime_struct.tm_year
+                    monthyear = str(month) + " " + str(year)
+
+                    if str(day) in day_dict:
+                        day_dict[str(day)] = day_dict[str(day)] + 1
+                    else: 
+                        day_dict[str(day)] = 1
+
+                    if str(month) in month_dict:
+                        month_dict[str(month)] = month_dict[str(month)] + 1
+                    else: 
+                        month_dict[str(month)] = 1
+                        
+                    if str(year) in year_dict:
+                        year_dict[str(year)] = year_dict[str(year)] + 1
+                    else: 
+                        year_dict[str(year)] = 1
+
             except AttributeError:
                 #print("Found no EXIF header... Skipping picture %s"%full_file)
                 print(".")
@@ -120,6 +151,10 @@ print("Apertures: %s\n"%aperture_dict)
 print("Exposures: %s\n"%exposure_dict)
 print("Focallength: %s\n"%focallength_dict)
 print("ISOs: %s\n"%iso_dict)
+print("Years: %s\n"%year_dict)
+print("Month: %s\n"%month_dict)
+print("Day: %s\n"%day_dict)
+
 
 write_csv_from_dict(camera_dict, "cameras.csv")
 write_csv_from_dict(iso_dict, "iso.csv")
@@ -134,3 +169,6 @@ cairoplot.pie_plot("apertures", aperture_dict, 500, 500, background = background
 cairoplot.pie_plot("exposures", exposure_dict, 500, 500, background = background, gradient = True, shadow = True)
 cairoplot.pie_plot("focallength", focallength_dict, 500, 500, background = background, gradient = True, shadow = True)
 cairoplot.pie_plot("iso", iso_dict, 500, 500, background = background, gradient = True, shadow = True)
+cairoplot.pie_plot("day", day_dict, 500, 500, background = background, gradient = True, shadow = True)
+cairoplot.pie_plot("year", year_dict, 500, 500, background = background, gradient = True, shadow = True)
+cairoplot.pie_plot("month", month_dict, 500, 500, background = background, gradient = True, shadow = True)
